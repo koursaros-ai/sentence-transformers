3a991f2fa74a3204b9b8c60c0f40739949e9b86f
Jack Pertschuk <jp954@cornell.edu>
2019-10-16 16:06:48 -0400

    .

--- sentence_transformers/QueryTransformer.py
+++ sentence_transformers/QueryTransformer.py
@@ -8,6 +8,8 @@ class QueryTransformer(nn.Sequential):
 
     def __init__(self, sentence_transformer : SentenceTransformer, embedding_size):
 
-        modules = [ ('sentence_transformer', sentence_transformer),
-                    ('feedforward_layer' , torch.nn.Linear(embedding_size, embedding_size)) ]
+        modules = OrderedDict([
+            ('sentence_transformer', sentence_transformer),
+            ('feedforward_layer' , torch.nn.Linear(embedding_size, embedding_size))])
+
         super().__init__(modules)
